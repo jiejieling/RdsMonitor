17beb29e0c15aa7e4b090ca4c7b2aff9d941695c
Seven Ling <sevenling@shijiebang.net>
2016-12-22 17:42:38 +0800

    v0.1 1. add daemon mode 2. optimize CPU load

--- src/redis-monitor.py
+++ src/redis-monitor.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+#-*-coding:utf8-*-
 
 from api.util import settings
 from dataprovider.dataprovider import RedisLiveDataProvider
@@ -12,286 +13,323 @@ import time
 
 
 class Monitor(object):
-    """Monitors a given Redis server using the MONITOR command.
-    """
+	"""Monitors a given Redis server using the MONITOR command.
+	"""
 
-    def __init__(self, connection_pool):
-        """Initializes the Monitor class.
+	def __init__(self, connection_pool):
+		"""Initializes the Monitor class.
 
-        Args:
-            connection_pool (redis.ConnectionPool): Connection pool for the \
-                    Redis server to monitor.
-        """
-        self.connection_pool = connection_pool
-        self.connection = None
+		Args:
+			connection_pool (redis.ConnectionPool): Connection pool for the \
+					Redis server to monitor.
+		"""
+		self.connection_pool = connection_pool
+		self.connection = None
 
-    def __del__(self):
-        try:
-            self.reset()
-        except:
-            pass
+	def __del__(self):
+		try:
+			self.reset()
+		except:
+			pass
 
-    def reset(self):
-        """If we have a connection, release it back to the connection pool.
-        """
-        if self.connection:
-            self.connection_pool.release(self.connection)
-            self.connection = None
+	def reset(self):
+		"""If we have a connection, release it back to the connection pool.
+		"""
+		if self.connection:
+			self.connection_pool.release(self.connection)
+			self.connection = None
 
-    def monitor(self):
-        """Kicks off the monitoring process and returns a generator to read the
-        response stream.
-        """
-        if self.connection is None:
-            self.connection = self.connection_pool.get_connection('monitor', None)
-        self.connection.send_command("monitor")
-        return self.listen()
+	def monitor(self):
+		"""Kicks off the monitoring process and returns a generator to read the
+		response stream.
+		"""
+		if self.connection is None:
+			self.connection = self.connection_pool.get_connection('monitor', None)
+		self.connection.send_command("monitor")
+		return self.listen()
 
-    def parse_response(self):
-        """Parses the most recent responses from the current connection.
-        """
-        return self.connection.read_response()
+	def parse_response(self):
+		"""Parses the most recent responses from the current connection.
+		"""
+		return self.connection.read_response()
 
-    def listen(self):
-        """A generator which yields responses from the MONITOR command.
-        """
-        while True:
-            yield self.parse_response()
+	def listen(self):
+		"""A generator which yields responses from the MONITOR command.
+		"""
+		while True:
+			yield self.parse_response()
 
 class MonitorThread(threading.Thread):
-    """Runs a thread to execute the MONITOR command against a given Redis server
-    and store the resulting aggregated statistics in the configured stats
-    provider.
-    """
+	"""Runs a thread to execute the MONITOR command against a given Redis server
+	and store the resulting aggregated statistics in the configured stats
+	provider.
+	"""
 
-    def __init__(self, server, port, password=None):
-        """Initializes a MontitorThread.
+	def __init__(self, server, port, password=None):
+		"""Initializes a MontitorThread.
 
-        Args:
-            server (str): The host name or IP of the Redis server to monitor.
-            port (int): The port to contact the Redis server on.
+		Args:
+			server (str): The host name or IP of the Redis server to monitor.
+			port (int): The port to contact the Redis server on.
 
-        Kwargs:
-            password (str): The password to access the Redis host. Default: None
-        """
-        super(MonitorThread, self).__init__()
-        self.server = server
-        self.port = port
-        self.password = password
-        self.id = self.server + ":" + str(self.port)
-        self._stop = threading.Event()
+		Kwargs:
+			password (str): The password to access the Redis host. Default: None
+		"""
+		super(MonitorThread, self).__init__()
+		self.server = server
+		self.port = port
+		self.password = password
+		self.id = self.server + ":" + str(self.port)
+		self._stop = threading.Event()
 
-    def stop(self):
-        """Stops the thread.
-        """
-        self._stop.set()
+	def stop(self):
+		"""Stops the thread.
+		"""
+		self._stop.set()
 
-    def stopped(self):
-        """Returns True if the thread is stopped, False otherwise.
-        """
-        return self._stop.is_set()
+	def stopped(self):
+		"""Returns True if the thread is stopped, False otherwise.
+		"""
+		return self._stop.is_set()
 
-    def run(self):
-        """Runs the thread.
-        """
-        stats_provider = RedisLiveDataProvider.get_provider()
-        pool = redis.ConnectionPool(host=self.server, port=self.port, db=0,
-                                    password=self.password)
-        monitor = Monitor(pool)
-        commands = monitor.monitor()
+	def run(self):
+		"""Runs the thread.
+		"""
+		stats_provider = RedisLiveDataProvider.get_provider()
+		pool = redis.ConnectionPool(host=self.server, port=self.port, db=0,
+									password=self.password)
+		monitor = Monitor(pool)
+		commands = monitor.monitor()
 
-        for command in commands:
-            try:
-                parts = command.split(" ")
+		for command in commands:
+			try:
+				parts = command.split(" ")
 
-                if len(parts) == 1:
-                    continue
+				if len(parts) == 1:
+					continue
 
-                epoch = float(parts[0].strip())
-                timestamp = datetime.datetime.fromtimestamp(epoch)
+				epoch = float(parts[0].strip())
+				timestamp = datetime.datetime.fromtimestamp(epoch)
 
-                # Strip '(db N)' and '[N x.x.x.x:xx]' out of the monitor str
-                if (parts[1] == "(db") or (parts[1][0] == "["):
-                    parts = [parts[0]] + parts[3:]
+				# Strip '(db N)' and '[N x.x.x.x:xx]' out of the monitor str
+				if (parts[1] == "(db") or (parts[1][0] == "["):
+					parts = [parts[0]] + parts[3:]
 
-                command = parts[1].replace('"', '').upper()
+				command = parts[1].replace('"', '').upper()
 
-                if len(parts) > 2:
-                    keyname = parts[2].replace('"', '').strip()
-                else:
-                    keyname = None
+				if len(parts) > 2:
+					keyname = parts[2].replace('"', '').strip()
+				else:
+					keyname = None
 
-                if len(parts) > 3:
-                    # TODO: This is probably more efficient as a list
-                    # comprehension wrapped in " ".join()
-                    arguments = ""
-                    for x in xrange(3, len(parts)):
-                        arguments += " " + parts[x].replace('"', '')
-                    arguments = arguments.strip()
-                else:
-                    arguments = None
+				if len(parts) > 3:
+					# TODO: This is probably more efficient as a list
+					# comprehension wrapped in " ".join()
+					arguments = ""
+					for x in xrange(3, len(parts)):
+						arguments += " " + parts[x].replace('"', '')
+					arguments = arguments.strip()
+				else:
+					arguments = None
 
-                if not command == 'INFO' and not command == 'MONITOR':
-                    stats_provider.save_monitor_command(self.id, 
-                                                        timestamp, 
-                                                        command, 
-                                                        str(keyname), 
-                                                        str(arguments))
+				if not command == 'INFO' and not command == 'MONITOR':
+					stats_provider.save_monitor_command(self.id, 
+														timestamp, 
+														command, 
+														str(keyname), 
+														str(arguments))
 
-            except Exception, e:
-                tb = traceback.format_exc()
-                print "==============================\n"
-                print datetime.datetime.now()
-                print tb
-                print command
-                print "==============================\n"
+			except Exception, e:
+				tb = traceback.format_exc()
+				print "==============================\n"
+				print datetime.datetime.now()
+				print tb
+				print command
+				print "==============================\n"
 
-            if self.stopped():
-                break
+			if self.stopped():
+				break
 
 class InfoThread(threading.Thread):
-    """Runs a thread to execute the INFO command against a given Redis server
-    and store the resulting statistics in the configured stats provider.
-    """
+	"""Runs a thread to execute the INFO command against a given Redis server
+	and store the resulting statistics in the configured stats provider.
+	"""
 
-    def __init__(self, server, port, password=None):
-        """Initializes an InfoThread instance.
+	def __init__(self, server, port, password=None, interval = 10):
+		"""Initializes an InfoThread instance.
 
-        Args:
-            server (str): The host name of IP of the Redis server to monitor.
-            port (int): The port number of the Redis server to monitor.
+		Args:
+			server (str): The host name of IP of the Redis server to monitor.
+			port (int): The port number of the Redis server to monitor.
 
-        Kwargs:
-            password (str): The password to access the Redis server. \
-                    Default: None
-        """
-        threading.Thread.__init__(self)
-        self.server = server
-        self.port = port
-        self.password = password
-        self.id = self.server + ":" + str(self.port)
-        self._stop = threading.Event()
+		Kwargs:
+			password (str): The password to access the Redis server. \
+					Default: None
+		"""
+		threading.Thread.__init__(self)
+		self.server = server
+		self.port = port
+		self.password = password
+		self.interval = interval
+		self.id = self.server + ":" + str(self.port)
+		self._stop = threading.Event()
 
-    def stop(self):
-        """Stops the thread.
-        """
-        self._stop.set()
+	def stop(self):
+		"""Stops the thread.
+		"""
+		self._stop.set()
 
-    def stopped(self):
-        """Returns True if the thread is stopped, False otherwise.
-        """
-        return self._stop.is_set()
+	def stopped(self):
+		"""Returns True if the thread is stopped, False otherwise.
+		"""
+		return self._stop.is_set()
 
-    def run(self):
-        """Does all the work.
-        """
-        stats_provider = RedisLiveDataProvider.get_provider()
-        redis_client = redis.StrictRedis(host=self.server, port=self.port, db=0,
-                                        password=self.password)
+	def run(self):
+		"""Does all the work.
+		"""
+		stats_provider = RedisLiveDataProvider.get_provider()
+		redis_client = redis.StrictRedis(host=self.server, port=self.port, db=0,
+										password=self.password)
 
-        # process the results from redis
-        while not self.stopped():
-            try:
-                redis_info = redis_client.info()
-                current_time = datetime.datetime.now()
-                used_memory = int(redis_info['used_memory'])
+		# process the results from redis
+		while not self.stopped():
+			try:
+				redis_info = redis_client.info()
+				current_time = datetime.datetime.now()
+				used_memory = int(redis_info['used_memory'])
 
-                # used_memory_peak not available in older versions of redis
-                try:
-                    peak_memory = int(redis_info['used_memory_peak'])
-                except:
-                    peak_memory = used_memory
+				# used_memory_peak not available in older versions of redis
+				try:
+					peak_memory = int(redis_info['used_memory_peak'])
+				except:
+					peak_memory = used_memory
 
-                stats_provider.save_memory_info(self.id, current_time, 
-                                                used_memory, peak_memory)
-                stats_provider.save_info_command(self.id, current_time, 
-                                                 redis_info)
+				stats_provider.save_memory_info(self.id, current_time, 
+												used_memory, peak_memory)
+				stats_provider.save_info_command(self.id, current_time, 
+												 redis_info)
 
-                # databases=[]
-                # for key in sorted(redis_info.keys()):
-                #     if key.startswith("db"):
-                #         database = redis_info[key]
-                #         database['name']=key
-                #         databases.append(database)
+				# databases=[]
+				# for key in sorted(redis_info.keys()):
+				#	 if key.startswith("db"):
+				#		 database = redis_info[key]
+				#		 database['name']=key
+				#		 databases.append(database)
 
-                # expires=0
-                # persists=0
-                # for database in databases:
-                #     expires+=database.get("expires")
-                #     persists+=database.get("keys")-database.get("expires")
+				# expires=0
+				# persists=0
+				# for database in databases:
+				#	 expires+=database.get("expires")
+				#	 persists+=database.get("keys")-database.get("expires")
 
-                # stats_provider.SaveKeysInfo(self.id, current_time, expires, persists)
+				# stats_provider.SaveKeysInfo(self.id, current_time, expires, persists)
 
-                time.sleep(1)
+				time.sleep(self.interval)
 
-            except Exception, e:
-                tb = traceback.format_exc()
-                print "==============================\n"
-                print datetime.datetime.now()
-                print tb
-                print "==============================\n"
+			except Exception, e:
+				tb = traceback.format_exc()
+				print "==============================\n"
+				print datetime.datetime.now()
+				print tb
+				print "==============================\n"
 
 class RedisMonitor(object):
+	def __init__(self):
+		self.threads = []
+		self.active = True
 
-    def __init__(self):
-        self.threads = []
-        self.active = True
+	def run(self, duration, interval):
+		"""Monitors all redis servers defined in the config for a certain number
+		of seconds.
 
-    def run(self, duration):
-        """Monitors all redis servers defined in the config for a certain number
-        of seconds.
-
-        Args:
-            duration (int): The number of seconds to monitor for.
-        """
-        redis_servers = settings.get_redis_servers()
+		Args:
+			duration (int): The number of seconds to monitor for.
+		"""
+		redis_servers = settings.get_redis_servers()
 
 
-        for redis_server in redis_servers:
+		for redis_server in redis_servers:
+			redis_password = redis_server.get("password", '')
+			'''
+			monitor = MonitorThread(redis_server["server"], redis_server["port"], redis_password)
+			self.threads.append(monitor)
+			monitor.setDaemon(True)
+			monitor.start()
+			'''
+			
+			info = InfoThread(redis_server["server"], redis_server["port"], redis_password, interval)
+			self.threads.append(info)
+			info.setDaemon(True)
+			info.start()
 
-            redis_password = redis_server.get("password")
+		if duration:
+			Timer(duration, self.stop).start()
 
-            monitor = MonitorThread(redis_server["server"], redis_server["port"], redis_password)
-            self.threads.append(monitor)
-            monitor.setDaemon(True)
-            monitor.start()
+		try:
+			while self.active:
+				time.sleep(1)
+		except (KeyboardInterrupt, SystemExit):
+			self.stop()
+			t.cancel()
 
-            info = InfoThread(redis_server["server"], redis_server["port"], redis_password)
-            self.threads.append(info)
-            info.setDaemon(True)
-            info.start()
+	def stop(self):
+		"""Stops the monitor and all associated threads.
+		"""
+		
+		for t in self.threads:
+				t.stop()
+		self.active = False
 
-        t = Timer(duration, self.stop)
-        t.start()
 
-        try:
-            while self.active:
-                pass
-        except (KeyboardInterrupt, SystemExit):
-            self.stop()
-            t.cancel()
-
-    def stop(self):
-        """Stops the monitor and all associated threads.
-        """
-        if args.quiet==False:
-            print "shutting down..."
-        for t in self.threads:
-                t.stop()
-        self.active = False
-
+def daemon(log):
+	os.umask(0)
+	
+	pid = os.fork()        
+	if pid > 0:
+		sys.exit(0)
+	
+	os.setsid()
+	pid = os.fork()
+	if pid > 0:
+		sys.exit(0)
+	
+	for i in range(1024):
+		try:
+			os.close(i)
+		except:
+			continue
+	
+	sys.stdin = open("/dev/null", "w+")
+	sys.stdout = open(log, 'a')
+	sys.stderr = sys.stdout
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Monitor redis.')
-    parser.add_argument('--duration',
-                        type=int,
-                        help="duration to run the monitor command (in seconds)",
-                        required=True)
-    parser.add_argument('--quiet',
-                        help="do  not write anything to standard output",
-                        required=False,
-                        action='store_true')
-    args = parser.parse_args()
-    duration = args.duration
-    monitor = RedisMonitor()
-    monitor.run(duration)
+	parser = argparse.ArgumentParser(description='Monitor redis.')
+	parser.add_argument('--duration',
+						type = int,
+						help = "duration to run the monitor command (in seconds)",
+						required = False,
+						default = 0)
+
+	parser.add_argument('--interval',
+						type = int,
+						help = "interval to run the monitor command (in seconds)",
+						required = False,
+						default = 10)
+
+	parser.add_argument('--daemon',
+						action = 'store_true',
+						help = "run as a deamon server",
+						required = False)
+
+	parser.add_argument('--log',
+						type = string,
+						help = "special log file path(only used for daemon mode)",
+						required = False)
+	
+	args = parser.parse_args()
+	if args.daemon:
+		daemon(args.log if args.log else '/dev/null')
+	
+	monitor = RedisMonitor()
+	monitor.run(args.duration, args.interval)
